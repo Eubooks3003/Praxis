6b0b56046e05346c0af8a33377e21303ebeb2ee9
Eubooks3003 <89107670+Eubooks3003@users.noreply.github.com>
2023-04-04 16:57:48 -0400

    Update Stepper.ino

--- Stepper.ino
+++ Stepper.ino
@@ -20,12 +20,12 @@ Stepper myStepper = Stepper(stepsPerRevolution, 10, 11, 12,13);
 
 const int RED_OUT = 2;
 const int GREEN_OUT = 3;
-const int Conveyer_OUT = 4;
+const int CONVEYOR_OUT = 4;
 
 // Check to see if red or green are the most prominent colors
-bool isGreater(int color, int colors[]){
+bool isGreater(int color, int* colors){
   bool greater = true;
-  for (int i = 0; i < sizeof(colors)/sizeof(colors[0]); ++ i){
+  for (int i = 0; i < 6; ++ i){
     if (color < colors[i]){ // Violet and orange seem to be really finicky consider removing them.
       greater = false;
     }
@@ -42,13 +42,18 @@ bool isConveyer(int colors[]){
 
 
 void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+
+
   // Set the motor speed (RPMs):
+  
   myStepper.setSpeed(200);
-  myStepper.step(200);
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RED_OUT, OUTPUT);
   pinMode(GREEN_OUT, OUTPUT);
+  pinMode(CONVEYOR_OUT, OUTPUT);
 }
 
 bool start = true;
@@ -58,16 +63,19 @@ bool is_red = false;
 bool is_green = false;
 
 void loop() {
+  //Serial.println("HELLO");
   // Step one revolution in one direction:
+ 
   int currentTime = millis();
   if(start){
-    myStepper.step(2000);
+    myStepper.step(1000);
     myTime = millis();
     start = false;
   }
 
   if(currentTime - myTime < delayTime & is_red == false & is_green == false){ // Only run if within delay or color has not been found yet
    ams.startMeasurement(); //begin a measurement
+   //Serial.println("HELLO THERE");
   
   //wait till data is available
   bool rdy = false;
@@ -82,7 +90,10 @@ void loop() {
    int yellow = sensorValues[AS726x_YELLOW];
    int orange = sensorValues[AS726x_ORANGE];
 
+   //Serial.print("Red: ");Serial.println(red);
+
    int colors[6] ={red, orange, yellow, green, blue, violet};
+
 
   is_conveyer = isConveyer(colors);
   if(!is_conveyer){
@@ -91,19 +102,40 @@ void loop() {
   }
   if(is_red){
     digitalWrite(RED_OUT, HIGH);
-    digitalWrite(Conveyer_OUT, HIGH);
+    digitalWrite(CONVEYOR_OUT, HIGH);
+    Serial.println("red");
+  }
+  else{
+    digitalWrite(RED_OUT, LOW);
+    digitalWrite(CONVEYOR_OUT, LOW);
+    Serial.println("no reed");
   }
   if(is_green){
     digitalWrite(GREEN_OUT, HIGH);
-    digitalWrite(Conveyer_OUT, HIGH);
+    digitalWrite(CONVEYOR_OUT, HIGH);
+    Serial.println("grren");
   }
-   
+  else{
+    digitalWrite(GREEN_OUT, LOW);
+    digitalWrite(CONVEYOR_OUT, LOW);
+    Serial.println("nogreen");
   }
+   delay(5);
+  }
+  else{
+    digitalWrite(CONVEYOR_OUT, HIGH);
+    Stepper.setSpeed(0);
+    Serial.println("done");
+    delay(5);
+  }
+
+  if(currentTime - myTime >= delayTime) myStepper.step(0);
   
 
   //myTime = millis();
 
 
   // Step on revolution in the other direction:
+  
  
 }
